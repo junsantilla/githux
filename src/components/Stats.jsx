c3083a89780be644fd464512ce46eaaf0de49c90
junsantilla <junsantilla@live.com>
2022-12-01 17:25:07 +0800

    Display stats from api

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useContext } from "react";
+import GithubContext from "../context/github/GithubContext";
 import {
 	BiUserPlus,
 	BiUserCheck,
@@ -7,46 +8,51 @@ import {
 } from "react-icons/bi";
 
 function Stats() {
+	const { user } = useContext(GithubContext);
+
+	const { followers, following, public_repos, public_gists } = user;
+
 	return (
 		<>
 			<div className="flex justify-center">
 				<div className="w-full max-w-5xl self-center px-2">
 					<div className="stats grid grid-cols-2 md:grid-cols-4 grid-flow-row rounded-none">
 						<div className="stat ">
-							<div className="stat-title">
-								<BiUserPlus className="inline mb-1" /> Followers
+							<div className="font-bold">
+								<BiUserPlus className="inline mb-1 " />{" "}
+								Followers
 							</div>
 							<div className="stat-value text-primary text-3xl">
-								25.6K
+								{followers}
 							</div>
 						</div>
 
 						<div className="stat ">
-							<div className="stat-title">
+							<div className="font-bold">
 								<BiUserCheck className="inline mb-1" />{" "}
 								Following
 							</div>
 							<div className="stat-value text-primary text-3xl">
-								2.6M
+								{following}
 							</div>
 						</div>
 
 						<div className="stat ">
-							<div className="stat-title">
+							<div className="font-bold">
 								<BiBookBookmark className="inline mb-1" />{" "}
 								Public Repositories
 							</div>
 							<div className="stat-value text-primary text-3xl">
-								2.6M
+								{public_repos}
 							</div>
 						</div>
 						<div className="stat ">
-							<div className="stat-title">
+							<div className="font-bold">
 								<BiBookmarkAlt className="inline mb-1" /> Public
 								Gists
 							</div>
 							<div className="stat-value text-primary text-3xl">
-								2.6M
+								{public_gists}
 							</div>
 						</div>
 					</div>
