8c1f7fc4bcfccc8b180cbe579d5b61a01da442da
junsantilla <junsantilla@live.com>
2022-12-02 07:58:43 +0800

    Move router and provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter as Rounter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
 	return (
-		<div className="App bg-black">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/users/:login" element={<Profile />} />
-			</Routes>
-		</div>
+		<Rounter>
+			<GithubProvider>
+				<div className="App bg-black">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/users/:login" element={<Profile />} />
+					</Routes>
+				</div>
+			</GithubProvider>
+		</Rounter>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Rounter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { GithubProvider } from "./context/github/GithubContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Rounter>
-			<GithubProvider>
-				<App />
-			</GithubProvider>
-		</Rounter>
+		<App />
 	</React.StrictMode>
 );
